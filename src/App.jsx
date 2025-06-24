2aa88ccfc3269e8d71f1614e1c5702018fe83413
mhmdkh1905 <mhmd52kh@gmail.com>
2025-06-24 19:21:31 +0300

    Project is done!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 // App.jsx
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "./layouts/Header";
 import Footer from "./layouts/Footer";
+import Header from "./layouts/header";
 
 
 const App = () => (
